cf83e1c85e0e15b67c1a2f4b4768582901e2f1b9
Abhijeet Solanki <abhijeetkumar824216@gmail.com>
2026-09-08 23:48:57 +0530

    0104-MAXIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 3 ms (4.47%), Space: 47.1 MB (26.3%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+
+        int height = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode nd = q.poll();
+
+                if (nd.left != null)   q.offer(nd.left);
+                if (nd.right != null)  q.offer(nd.right);
+            }
+              height++ ;
+        }
+
+        return height;
+
+    }
+}
--- README.md
+++ README.md
@@ -13,4 +13,20 @@ A collection of LeetCode questions to ace the coding interviews! - Created using
 |  |
 | ------- |
 | [0503-next-greater-element-ii](https://github.com/Abhijeetkumar4525/DSA/tree/master/0503-next-greater-element-ii) |
+## Tree
+|  |
+| ------- |
+| [0104-maximum-depth-of-binary-tree](https://github.com/Abhijeetkumar4525/DSA/tree/master/0104-maximum-depth-of-binary-tree) |
+## Depth-First Search
+|  |
+| ------- |
+| [0104-maximum-depth-of-binary-tree](https://github.com/Abhijeetkumar4525/DSA/tree/master/0104-maximum-depth-of-binary-tree) |
+## Breadth-First Search
+|  |
+| ------- |
+| [0104-maximum-depth-of-binary-tree](https://github.com/Abhijeetkumar4525/DSA/tree/master/0104-maximum-depth-of-binary-tree) |
+## Binary Tree
+|  |
+| ------- |
+| [0104-maximum-depth-of-binary-tree](https://github.com/Abhijeetkumar4525/DSA/tree/master/0104-maximum-depth-of-binary-tree) |
 <!---LeetCode Topics End-->
